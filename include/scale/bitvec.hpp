59aa4769d167daece72620d094dbac3a9fcc180a
turuslan <turuslan.devbox@gmail.com>
2022-09-07 11:46:21 +0300

    fix comment

--- include/scale/bitvec.hpp
+++ include/scale/bitvec.hpp
@@ -10,7 +10,7 @@
 
 namespace scale {
   /**
-   * @brief convenience alias for arrays of bytes
+   * @brief Bit vector encoding compatible with rust `BitVec<u8, Lsb0>`
    */
   struct BitVec {
     std::vector<bool> bits;
