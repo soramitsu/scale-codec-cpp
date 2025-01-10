160d1a15b677cb2381397a4fbe1fb638cb3559ee
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-01-10 14:41:50 +0800

    refactor: rename `test` feature for unifying
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/qtils/portfile.cmake
+++ vcpkg-overlay/qtils/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/qtils
-  REF 58dd0cbd7a1ae1cad3b30cb5390080931d5b3368
-  SHA512 47dbac166beaeaa91dc74687540a87f85b8645cb0dfb9940abbfc4c5fdfe9fd7888ee8948bd7d0afe6ea8b3842ddc50a2e344704e12173dddc1c587ce4cd7f04
+  REF 80d80fa842fae36ed5926254cee032d5b0f37c7d
+  SHA512 136c9192cd8d503bc32a7bd541f82f5194de94c7684d09407ca6a506419ae160e42a479fe4ceb64d14cd2a4c8fc85eb3390ce089a4054bbad55e2c9fd0820c8e
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
